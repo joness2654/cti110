515e717334d8aa9f8d26d9574f9e040373f69a6a
joness2654 <joness2654@student.faytechcc.edu>
2025-06-27 21:08:26 -0400

    Add files via upload
    First Submission

---
+++ P1LAB1_JonesSkye.py
@@ -0,0 +1,15 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print ('Skye Jones')
+Skye Jones
+>>> print ('June 24th 2025')
+June 24th 2025
+>>> print ('P1LAB1')
+P1LAB1
+>>> print ('Python is opening reading deleting files')
+Python is opening reading deleting files
+>>> 
+============== RESTART: C:/Users/Bleu/Desktop/P1LAB1_JonesSkye.py ==============
+Skye
+Jones
+Hello, Skye Jones! Welcome to CTI-110
