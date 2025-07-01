925081d0473088346fe1ffc0dceed0efbbb9daf2
joness2654 <joness2654@student.faytechcc.edu>
2025-07-01 18:52:13 -0400

    Add files via upload

---
+++ P1HW2_JonesSkye.py
@@ -0,0 +1,10 @@
+Python 3.14.0b3 (tags/v3.14.0b3:26d485d, Jun 17 2025, 16:21:24) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Skye Jones")
+Skye Jones
+>>> print("June 30th, 2025")
+June 30th, 2025
+>>> print("CTI-110 P1HW2")
+CTI-110 P1HW2
+>>> print("Creating progams doing simple math")
+Creating progams doing simple math
