b218cac0063d6e84915b35ff27805212f4e42c9c
joness2654 <joness2654@student.faytechcc.edu>
2025-07-01 06:52:16 -0400

    Add files via upload

---
+++ P1HW1_JonesSkye.py
@@ -0,0 +1,47 @@
+Python 3.14.0b3 (tags/v3.14.0b3:26d485d, Jun 17 2025, 16:21:24) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("Skye Mercer")
+Skye Mercer
+>>> print("June 30th, 2025")
+June 30th, 2025
+>>> print("CTI-110 P1HW1")
+CTI-110 P1HW1
+>>> print("A short description on this project is, you will learn how to creat programs that show output, input and print functions all using Python's while walking through the text.")
+A short description on this project is, you will learn how to creat programs that show output, input and print functions all using Python's while walking through the text.
+>>> print("-----Calculating Exponenets----")
+-----Calculating Exponenets----
+>>> print("Enter an integer as the base value: 7")
+...       
+... Enter an integer as the base value: 7
+... print("Enter an integer as the exponent: 3")
+...       
+... Enter an integer as the exponent: 3
+... 
+... 
+... print("7 raised to the power of 3 is 343 !!")
+...       
+... 7 raised to the power of 3 is 343 !!
+... 
+... 
+... print("-----Addition and Subtraction----")
+...       
+... -----Addition and Subtraction----
+... 
+... 
+... 
+... 
+... print("Enter a starting integer: 10")
+...       
+... Enter a starting integer: 10
+print("Enter an integer to add: 6")
+      
+Enter an integer to add: 6
+print("Enter an integer to subtract: 4")
+      
+Enter an integer to subtract: 4
+
+
+
+print("10 + 6 - 4 is equal to 12")
+      
+10 + 6 - 4 is equal to 12
